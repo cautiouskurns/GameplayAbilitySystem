6356fa0e9984b68e46fc6802b0c7be232ea961d6
cautiouskurns <114108720+cautiouskurns@users.noreply.github.com>
2024-04-29 20:39:55 +0100

    Auro Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,14 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
 };
